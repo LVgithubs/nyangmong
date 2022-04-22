cd480cce69beb7bb82df010b2b145abb6425d0b4
fwangjuwon <fwangjuwon@naver.com>
2022-04-22 11:30:11 +0900

    [황주원] 관리자코드 생성 및 체크박스로 댓글 삭제구현

--- src/main/java/spring/project/nyangmong/web/AdminController.java
+++ src/main/java/spring/project/nyangmong/web/AdminController.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
