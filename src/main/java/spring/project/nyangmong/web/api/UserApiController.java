d8f865e7707ba0e4b91fef37d1bc49531694966f
gih1214 <gih1214@hanmail.net>
2022-04-22 19:32:52 +0900

    [구아현] User 인증 및 권한 처리

--- src/main/java/spring/project/nyangmong/web/api/UserApiController.java
+++ src/main/java/spring/project/nyangmong/web/api/UserApiController.java
@@ -24,20 +24,27 @@ public class UserApiController {
     // 회원 탈퇴
     @DeleteMapping("/s/api/user/{id}/delete")
     public @ResponseBody ResponseDto<?> userDelete(@PathVariable Integer id) {
-        User principal = (User) session.getAttribute("principal");
-        if (principal != null) {
-            userService.회원탈퇴(id);
-            session.invalidate();
 
-            return new ResponseDto<>(1, "성공", null);
+        // 권한 - 세션의 아이디와 {id}를 비교
+        User principal = (User) session.getAttribute("principal");
+        if (principal.getId() != id) {
+            throw new RuntimeException("삭제 권한이 없습니다.");
         } else {
-            return new ResponseDto<>(-1, "실패", null);
+            return new ResponseDto<>(1, "성공", null);
         }
+
     }
 
     // 회원 정보 수정
     @PutMapping("/s/api/user/{id}/update")
     public ResponseDto<?> update(@PathVariable Integer id, @RequestBody UpdateDto updateDto) {
+
+        // 권한 - 세션의 아이디와 {id}를 비교
+        User principal = (User) session.getAttribute("principal");
+        if (principal.getId() != id) {
+            throw new RuntimeException("수정 권한이 없습니다.");
+        }
+
         User userEntity = userService.회원수정(id, updateDto);
         session.setAttribute("principal", userEntity); // 세션 변경하기
         return new ResponseDto<>(1, "성공", null);
